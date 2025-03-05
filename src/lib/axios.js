7c0281daeae67d0c37a573843112677f146a5db7
Ashish <ashishdhola0.7@gmail.com>
2025-03-05 15:30:21 +0530

    added

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-server-h4qa.onrender.com/api",
+  // baseURL: "https://chat-server-h4qa.onrender.com/api",
+  baseURL: "https://chat-server-h4qa.onrender.com",
   
   withCredentials: true,
 });
