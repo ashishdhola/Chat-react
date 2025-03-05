6a8965a00ea7321cc09ad4b452d357c16b24d514
Ashish <ashishdhola0.7@gmail.com>
2025-03-05 14:59:10 +0530

    errore-solve

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://172.19.3.48:5001/api",
-  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: "http://localhost:5001/api",
   
   withCredentials: true,
 });
