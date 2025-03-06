0a0ad530054e7077579c68e2d3763b2042683a21
Ashish <ashishdhola0.7@gmail.com>
2025-03-06 15:15:27 +0530

    edded

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://chat-server-h4qa.onrender.com/api",
-  
-  withCredentials: true,
+  withCredentials: true, 
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
